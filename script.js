ad39e0e920e03b5c44d1c71b21f71779f25a245a
Stephanie Aurelio <stephanie.m.aurelio@gmail.com>
2019-10-03 15:25:57 -0700

    updated javascript

--- script.js
+++ script.js
@@ -7,6 +7,24 @@
 //      4. Generate a string of the length associated with the three options
 //      5. Auto-fill the text area box
 // BONUS: code the copy to clipboard button
+var complexity = prompt('What is your desired password length? Enter a number between 8-26');
+console.log(complexity);
 
-var passwordGood, passwordStrong;
+var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*?";
 
+document.querySelector('.btn-generate').addEventListener('click', function() {
+    for(var i = 0; i <= complexity; i++){
+    var password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+    var passwordDOM = document.querySelector('.generated-password');
+    passwordDOM.textContent = password;
+    passwordDOM.style.color = 'black';
+    passwordDOM.style.fontSize = '24px'
+});
+
+document.querySelector('.btn-clipboard').addEventListener('click', function(){
+    var copyPassword = document.getElementById('.generated-password');
+    document.execCommand("Copy");
+    alert("Password copied!");
+    });
+   
