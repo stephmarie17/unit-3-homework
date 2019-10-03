907c643054fbd6c87d01f480561d6baac26b9dca
Stephanie Aurelio <stephanie.m.aurelio@gmail.com>
2019-10-03 13:50:11 -0700

    created index, script, and style files

---
+++ script.js
@@ -0,0 +1,12 @@
+// On page load, dialog box asks user:
+// What password strength do you want to generate? Please enter: Good or Strong
+// Depending on strength selected, code will:
+//      1. Randomize numbers
+//      2. Randomize letters
+//      3. If needed, randomize special characters
+//      4. Generate a string of the length associated with the three options
+//      5. Auto-fill the text area box
+// BONUS: code the copy to clipboard button
+
+var passwordGood, passwordStrong;
+
