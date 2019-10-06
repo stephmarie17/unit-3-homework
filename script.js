bcceb180e8ff19cb42e05db3ee9a09c1fe59fc1a
Stephanie Aurelio <stephanie.m.aurelio@gmail.com>
2019-10-05 18:03:33 -0700

    edited script

--- script.js
+++ script.js
@@ -2,22 +2,6 @@
 
 var criteriaLength, criteriaAlphaUc, criteriaAlphaLc, criteriaAlphaMix, criteriaNumbers, criteriaSpecial, criteriaNumSecMix, criteriaAlphaUcNumbers, criteriaAlphaLcNumbers, criteriaAlphaNumMix, criteriaAlphaNumSpecMix, copyPassword, passwordDOM;
 
-var password = "";
-
-// Value variables
-var valuesAlphaUc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var valuesAlphaLc = 'abcdefghijklmnopqrstuvwxyz';
-var valuesAlphaMix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-
-var valuesNumbers = 123456789;
-var valuesSpecial = '!@#$%^&*?';
-var valuesNumSpecMix = '123456789!@#$%^&*?'
-
-var valuesAlphaUcNumbers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
-var valuesAlphaLcNumbers = 'abcdefghijklmnopqrstuvwxyz123456789';
-var valuesAlphaNumMix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789';
-var valuesAlphaNumSpecMix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*?';
-
 // Declare function/s
 function criteriaPassword() {
     var criteriaLength = prompt('What is your desired password length? Enter a number between 8-128');
@@ -33,41 +17,58 @@ function criteriaPassword() {
 }
 
 function generatePassword() {
-for (var i = 1; i <= criteriaLength; i++) {
+var password = "";
+// Value variables
+var valuesAlphaUc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var valuesAlphaLc = 'abcdefghijklmnopqrstuvwxyz';
+var valuesAlphaMix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+var valuesNumbers = 123456789;
+var valuesSpecial = '!@#$%^&*?';
+var valuesNumSpecMix = '123456789!@#$%^&*?'
+
+var valuesAlphaUcNumbers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+var valuesAlphaLcNumbers = 'abcdefghijklmnopqrstuvwxyz123456789';
+var valuesAlphaNumMix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789';
+var valuesAlphaNumSpecMix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*?';
+
+for (var i=0; i <= criteriaLength; i++) {
     if (criteriaAlphaUc== 'Y') {
-        password = password + valuesAlphaUc.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaUc.length - 1)));
+        password += valuesAlphaUc.charAt(Math.floor(Math.random() * valuesAlphaUc.length - 1));
     }
     else if (criteriaAlphaLc== 'Y') {
-        password = password + valuesAlphaLc.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaLc.length - 1)));
+        password += valuesAlphaLc.charAt(Math.floor(Math.random() * valuesAlphaLc.length - 1));
     }
     else if (criteriaAlphaLc== 'Y' && criteriaAlphaUc== 'Y') {
-        password = password + valuesAlphaMix.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaMix.length - 1)));
+        password += valuesAlphaMix.charAt(Math.floor(Math.random() * valuesAlphaMix.length - 1));
     }
     else if (criteriaAlphaUc== 'Y' && criteriaNumbers== 'Y') {
-        password = password + valuesAlphaUcNumbers.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaUcNumbers.length - 1)));
+        password += valuesAlphaUcNumbers.charAt(Math.floor(Math.random() * valuesAlphaUcNumbers.length - 1));
     }
     else if (criteriaAlphaLc== 'Y' && criteriaNumbers== 'Y') {
-        password = password + valuesAlphaLcNumbers.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaLcNumbers.length - 1)));
+        password += valuesAlphaLcNumbers.charAt(Math.floor(Math.random() * valuesAlphaLcNumbers.length - 1));
     }
     else if (criteriaAlphaLc== 'Y' && criteriaAlphaUc=='Y' && criteriaNumbers== 'Y') {
-        password = password + valuesAlphaNumMix.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaNumMix.length - 1)));
+        password += valuesAlphaNumMix.charAt(Math.floor(Math.random() * valuesAlphaNumMix.length - 1));
     }
     else if (criteriaAlphaLc== 'Y' && criteriaAlphaUc== 'Y' && criteriaNumbers== 'Y' && criteriaSpecial== 'Y') {
-        password = password + valuesAlphaNumSpecMix.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaNumSpecMix.length - 1)));
+        password += valuesAlphaNumSpecMix.charAt(Math.floor(Math.random() * valuesAlphaNumSpecMix.length - 1));
     }
     else if (criteriaNumbers== 'Y') {
-        password = password + valuesNumbers.charAt(Math.floor(Math.random() * Math.floor(valuesNumbers.length - 1)));
+        password += valuesNumbers.charAt(Math.floor(Math.random() * valuesNumbers.length - 1));
     }
     else if (criteriaSpecial== 'Y') {
-        password = password + valuesSpecial.charAt(Math.floor(Math.random() * Math.floor(valuesSpecial.length - 1)));
+        password += valuesSpecial.charAt(Math.floor(Math.random() * valuesSpecial.length - 1));
     }
     else if (criteriaNumbers== 'Y' && criteriaSpecial== 'Y') {
-        password = password + valuesNumSpecMix.charAt(Math.floor(Math.random() * Math.floor(valuesNumSpecMix.length - 1)));
+        password += valuesNumSpecMix.charAt(Math.floor(Math.random() * valuesNumSpecMix.length - 1));
     }
-    console.log(password);
+    return password;
     }
     var passwordDOM = document.querySelector('#generated-password');
+
     // Style for generated password
+
     passwordDOM.textContent = password;
     passwordDOM.style.color = 'black';
     passwordDOM.style.fontSize = '24px';
