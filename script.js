cda186aec08cd3527b24e3a7f7c9985455763e3d
Stephanie A <stephanie.m.aurelio@gmail.com>
2019-11-12 12:50:39 -0800

    fixed logic so that javascript works correctly

--- script.js
+++ script.js
@@ -1,107 +1,261 @@
 // Declare global variables
+var passwordDisplay = document.querySelector('.password-box.sm');
+var criteriaResponses = [];
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbers = '0123456789';
+var specialChar = '!@#$%^&*()?';
 
-var criteriaLength, criteriaAlphaUc, criteriaAlphaLc, criteriaAlphaMix, criteriaNumbers, criteriaSpecial, criteriaNumSecMix, criteriaAlphaUcNumbers, criteriaAlphaLcNumbers, criteriaAlphaNumMix, criteriaAlphaNumSpecMix, password, copyPassword, passwordDOM;
+var lowerUpperMix = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerNumMix = 'abcdefghijklmnopqrstuvwxyz0123456789';
+var lowerNumCharMix = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()?0123456789';
+var lowerCharMix = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()?';
+var upperNumMix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+var upperNumCharMix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?0123456789';
+var upperCharMix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?';
+var numCharMix = '0123456789!@#$%^&*()?';
+var allMix = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()?';
 
-// Declare function/s
-function criteriaPassword() {
-    var criteriaLength = prompt('What is your desired password length? Enter a number between 8-128');
-        console.log(criteriaLength);
-        // if(criteriaLength < 8 || criteriaLength > 128)
-        // {
-        //     alert('Password must be between 8-128 characters.')
-        // } 
-    var criteriaAlphaUc = prompt('Would you like uppercase letters? Y/N');
-        console.log(criteriaAlphaUc);
-    var criteriaAlphaLc = prompt('Would you like lowercase letters? Y/N');
-        console.log(criteriaAlphaLc);
-    var criteriaNumbers = prompt('Would you like numbers? Y/N');
-        console.log(criteriaNumbers);
-    var criteriaSpecial = prompt('Would you like special characters? Y/N');
-        console.log(criteriaSpecial);
+// Begins the criteria selection process, pushing responses into an array
+function generatorInit() {
+    var element = document.getElementById('password-box-text');
+    element.innerHTML = "How long would you like your password to be? Enter a number between 8-128.";
+    var input = document.createElement('textarea');
+    var nextBtn = document.createElement('button');
+    input.name = 'length';
+    input.maxLength = '3';
+    input.cols = '5';
+    input.rows = '5';
+    nextBtn.setAttribute('class','btn');
+    nextBtn.innerHTML = 'Next';
+    element.appendChild(input);
+    element.appendChild(nextBtn);
+
+    document.querySelector('.btn').addEventListener('click', function() {
+        var number = input.value;
+        criteriaResponses.push(number);
+        console.log(criteriaResponses);
+        criteriaLC();
+    });
+};
+
+function criteriaLC() {
+    var element = document.getElementById('password-box-text');
+    element.innerHTML = "Would you like lowercase letters?"
+    var radioYes = document.createElement('input');
+    radioYes.setAttribute('type', 'radio');
+    radioYes.setAttribute('id', 'radioYes');
+    radioYes.setAttribute('value', 'Yes');
+    radioYes.setAttribute('name', 'choice');
+    var labelYes = document.createElement('label');
+    labelYes.innerHTML = 'Yes';
+    element.appendChild(labelYes);
+    element.appendChild(radioYes);
+
+    var radioNo = document.createElement('input');
+    radioNo.setAttribute('type', 'radio');
+    radioNo.setAttribute('id', 'radioNo');
+    radioNo.setAttribute('value', 'No');
+    radioNo.setAttribute('name', 'choice');
+    var labelNo = document.createElement('label');
+    labelNo.innerHTML = 'No';
+    element.appendChild(labelNo);
+    element.appendChild(radioNo);
+
+    var nextBtn = document.createElement('button');
+    nextBtn.setAttribute('class','btn');
+    nextBtn.innerHTML = 'Next';
+    element.appendChild(nextBtn);
+
+    document.querySelector('.btn').addEventListener('click', function() {
+        var criteriaLc = document.querySelector('input[name="choice"]:checked').value;
+        criteriaResponses.push(criteriaLc);
+        console.log(criteriaResponses);
+        criteriaUC();
+    });
+;}
+
+function criteriaUC() {
+    var element = document.getElementById('password-box-text');
+    element.innerHTML = "Would you like UPPERCASE LETTERS?"
+    var radioYes = document.createElement('input');
+    radioYes.setAttribute('type', 'radio');
+    radioYes.setAttribute('id', 'radioYes');
+    radioYes.setAttribute('value', 'Yes');
+    radioYes.setAttribute('name', 'choice');
+    var labelYes = document.createElement('label');
+    labelYes.innerHTML = 'Yes';
+    element.appendChild(labelYes);
+    element.appendChild(radioYes);
+
+    var radioNo = document.createElement('input');
+    radioNo.setAttribute('type', 'radio');
+    radioNo.setAttribute('id', 'radioNo');
+    radioNo.setAttribute('value', 'No');
+    radioNo.setAttribute('name', 'choice');
+    var labelNo = document.createElement('label');
+    labelNo.innerHTML = 'No';
+    element.appendChild(labelNo);
+    element.appendChild(radioNo);
+
+    var nextBtn = document.createElement('button');
+    nextBtn.setAttribute('class','btn');
+    nextBtn.innerHTML = 'Next';
+    element.appendChild(nextBtn);
+
+    document.querySelector('.btn').addEventListener('click', function() {
+        var criteriaUc = document.querySelector('input[name="choice"]:checked').value;
+        criteriaResponses.push(criteriaUc);
+        console.log(criteriaResponses);
+        criteriaNum();
+    });
+};
+
+function criteriaNum() {
+    var element = document.getElementById('password-box-text');
+    element.innerHTML = "Would you like numbers?"
+    var radioYes = document.createElement('input');
+    radioYes.setAttribute('type', 'radio');
+    radioYes.setAttribute('id', 'radioYes');
+    radioYes.setAttribute('value', 'Yes');
+    radioYes.setAttribute('name', 'choice');
+    var labelYes = document.createElement('label');
+    labelYes.innerHTML = 'Yes';
+    element.appendChild(labelYes);
+    element.appendChild(radioYes);
+
+    var radioNo = document.createElement('input');
+    radioNo.setAttribute('type', 'radio');
+    radioNo.setAttribute('id', 'radioNo');
+    radioNo.setAttribute('value', 'No');
+    radioNo.setAttribute('name', 'choice');
+    var labelNo = document.createElement('label');
+    labelNo.innerHTML = 'No';
+    element.appendChild(labelNo);
+    element.appendChild(radioNo);
+
+    var nextBtn = document.createElement('button');
+    nextBtn.setAttribute('class','btn');
+    nextBtn.innerHTML = 'Next';
+    element.appendChild(nextBtn);
+
+    document.querySelector('.btn').addEventListener('click', function() {
+        var criteriaNum = document.querySelector('input[name="choice"]:checked').value;
+        criteriaResponses.push(criteriaNum);
+        console.log(criteriaResponses);
+        criteriaChar();
+    });
+};
+
+function criteriaChar() {
+    var element = document.getElementById('password-box-text');
+    element.innerHTML = "Would you like special characters?"
+    var radioYes = document.createElement('input');
+    radioYes.setAttribute('type', 'radio');
+    radioYes.setAttribute('id', 'radioYes');
+    radioYes.setAttribute('value', 'Yes');
+    radioYes.setAttribute('name', 'choice');
+    var labelYes = document.createElement('label');
+    labelYes.innerHTML = 'Yes';
+    element.appendChild(labelYes);
+    element.appendChild(radioYes);
+
+    var radioNo = document.createElement('input');
+    radioNo.setAttribute('type', 'radio');
+    radioNo.setAttribute('id', 'radioNo');
+    radioNo.setAttribute('value', 'No');
+    radioNo.setAttribute('name', 'choice');
+    var labelNo = document.createElement('label');
+    labelNo.innerHTML = 'No';
+    element.appendChild(labelNo);
+    element.appendChild(radioNo);
+
+    var nextBtn = document.createElement('button');
+    nextBtn.setAttribute('class','btn');
+    nextBtn.innerHTML = 'Finish';
+    element.appendChild(nextBtn);
+
+    document.querySelector('.btn').addEventListener('click', function() {
+        var criteriaChar = document.querySelector('input[name="choice"]:checked').value;
+        criteriaResponses.push(criteriaChar);
+        console.log(criteriaResponses);      
+        generatePassword();
+    });
+};
+
+function generatePassword() {
+    var numberLength = parseInt(criteriaResponses[0]);
+    var password = "";
+    for (var i = 0; i < numberLength; i++) {
+        if (criteriaResponses[1] === "Yes" && criteriaResponses[2] === "Yes" && criteriaResponses[3] === "Yes" && criteriaResponses[4] === "Yes"){
+            password += allMix[Math.floor(Math.random() * allMix.length)];        
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = password;
+        } else if (criteriaResponses[1] === "Yes" && criteriaResponses[2] === "Yes" && criteriaResponses[3] === "No" && criteriaResponses[4] === "No"){
+            password += lowerUpperMix[Math.floor(Math.random() * lowerUpperMix.length)];
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = password;
+        } else if (criteriaResponses[1] === "Yes" && criteriaResponses[2] === "No" && criteriaResponses[3] === "Yes" && criteriaResponses[4] === "No"){
+            password += lowerNumMix[Math.floor(Math.random() * lowerNumMix.length)];
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = password;
+        } else if (criteriaResponses[1] === "Yes" && criteriaResponses[2] === "No" && criteriaResponses[3] === "No" && criteriaResponses[4] === "Yes"){
+            password += lowerCharMix[Math.floor(Math.random() * lowerCharMix.length)];
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = password;
+        } else if (criteriaResponses[1] === "Yes" && criteriaResponses[2] === "No" && criteriaResponses[3] === "No" && criteriaResponses[4] === "No"){
+            password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = password;
+        } else if (criteriaResponses[1] === "No" && criteriaResponses[2] === "Yes" && criteriaResponses[3] === "No" && criteriaResponses[4] === "No"){
+            password += upperCase[Math.floor(Math.random() * upperCase.length)];
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = password;
+        } else if (criteriaResponses[1] === "No" && criteriaResponses[2] === "Yes" && criteriaResponses[3] === "Yes" && criteriaResponses[4] === "No"){
+            password += upperNumMix[Math.floor(Math.random() * upperNumMix.length)];
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = password;
+        } else if (criteriaResponses[1] === "No" && criteriaResponses[2] === "Yes" && criteriaResponses[3] === "No" && criteriaResponses[4] === "Yes"){
+            password += upperCharMix[Math.floor(Math.random() * upperCharMix.length)];
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = password;
+        } else if (criteriaResponses[1] === "No" && criteriaResponses[2] === "No" && criteriaResponses[3] === "Yes" && criteriaResponses[4] === "Yes"){
+            password += numCharMix[Math.floor(Math.random() * numCharMix.length)];
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = password;
+        } else if (criteriaResponses[1] === "Yes" && criteriaResponses[2] === "No" && criteriaResponses[3] === "Yes" && criteriaResponses[4] === "Yes"){
+            password += lowerNumCharMix[Math.floor(Math.random() * lowerNumCharMix.length)];
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = password;
+        } else if (criteriaResponses[1] === "No" && criteriaResponses[2] === "Yes" && criteriaResponses[3] === "Yes" && criteriaResponses[4] === "Yes"){
+            password += upperNumCharMix[Math.floor(Math.random() * upperNumCharMix.length)];
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = password;
+        }else {
+            var element = document.getElementById('password-box-text');
+            element.innerHTML = "Your password cannot be generated with that criteria. Please start again.";
+        }
+    }
+};
+
+function clearArray() {
+    return criteriaResponses = [];
 }
-
-function generatePassword(criteriaLength, criteriaAlphaUc, criteriaAlphaLc, criteriaNumbers, criteriaSpecial) {
-
-var password = "";
-
-// Value variables
-var valuesAlphaUc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var valuesAlphaLc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var valuesAlphaMix = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var valuesNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var valuesSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
-var valuesNumSpecMix = [1, 2, 3, 4, 5, 6, 7, 8, 9, "!", "@", "#", "$", "%", "^", "&", "*", "?"];
-
-var valuesAlphaUcNumbers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var valuesAlphaLcNumbers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var valuesAlphaNumMix = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var valuesAlphaNumSpecMix = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, "!", "@", "#", "$", "%", "^", "&", "*", "?"];
-
-for(var i = 0; i <= criteriaLength; i++) {
-    if(criteriaAlphaUc== 'Y') {
-        password = password + valuesAlphaUc.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaUc.length - 1)));
-    }
-    else if(criteriaAlphaLc== 'Y') {
-        password = password + valuesAlphaLc.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaLc.length - 1)));
-    }
-    else if(criteriaAlphaLc== 'Y' && criteriaAlphaUc== 'Y') {
-        password = password + valuesAlphaMix.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaMix.length - 1)));
-    }
-    else if(criteriaAlphaUc== 'Y' && criteriaNumbers== 'Y') {
-        password = password + valuesAlphaUcNumbers.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaUcNumbers.length - 1)));
-    }
-    else if(criteriaAlphaLc== 'Y' && criteriaNumbers== 'Y') {
-        password = password + valuesAlphaLcNumbers.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaLcNumbers.length - 1)));
-    }
-    else if(criteriaAlphaLc== 'Y' && criteriaAlphaUc=='Y' && criteriaNumbers== 'Y') {
-        password = password + valuesAlphaNumMix.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaNumMix.length - 1)));
-    }
-    else if(criteriaAlphaLc== 'Y' && criteriaAlphaUc== 'Y' && criteriaNumbers== 'Y' && criteriaSpecial== 'Y') {
-        password = password + valuesAlphaNumSpecMix.charAt(Math.floor(Math.random() * Math.floor(valuesAlphaNumSpecMix.length - 1)));
-    }
-    else if(criteriaNumbers== 'Y') {
-        password = password + valuesNumbers.charAt(Math.floor(Math.random() * Math.floor(valuesNumbers.length - 1)));
-    }
-    else if(criteriaSpecial== 'Y') {
-        password = password + valuesSpecial.charAt(Math.floor(Math.random() * Math.floor(valuesSpecial.length - 1)));
-    }
-    else if(criteriaNumbers== 'Y' && criteriaSpecial== 'Y') {
-        password = password + valuesNumSpecMix.charAt(Math.floor(Math.random() * Math.floor(valuesNumSpecMix.length - 1)));
-    }
-    
-    console.log(password);
-    return password;
-
-    }
-
-    var passwordDOM = document.querySelector('#generated-password');
-
-    // Style for generated password
-
-    passwordDOM.textContent = password;
-    passwordDOM.style.color = 'black';
-    passwordDOM.style.fontSize = '24px';
-}   
 
 function copyPassword() {
-    copyPassword = document.getElementById('generated-password');
-    document.execCommand("copy");
-    alert("Password copied!");
-}
+    var copyText = document.getElementById('password-box-text');
+    document.execCommand("copy", copyText);
+    alert("Your password has been copied!");
+};
 
 // Call function/s
 
 document.querySelector('.btn-generate').addEventListener('click', function() {
-    criteriaPassword();
-    generatePassword();
+    generatorInit();
+    clearArray();
 });
 
 document.querySelector('.btn-clipboard').addEventListener('click', function() {
     copyPassword();
 });
+
